e09976c48da4e569a60412791f0b77e740f469c1
anastasia_lykova <anastasia.7@mail.ru>
2023-05-19 15:52:13 +0500

    function hide_card_number

--- utils/utils.py
+++ utils/utils.py
@@ -7,3 +7,22 @@ def load_json():
     json_path = os.path.join("..", "operations.json")
     with open(json_path, "r", encoding="utf-8") as file:
         return json.loads(file.read())
+
+
+def hide_card_number(unhidden_number):
+    """Принимает данные о счете или карте в формате строки вида:
+    счет номер/название карты номер
+    возвращает замаскированные данные счета или карты"""
+
+    split_description = unhidden_number.split(" ")
+
+    if len(split_description[-1]) == 20:
+        account_number = split_description[-1]
+        cut_account_number = account_number[-4:]
+        return f"{' '.join(split_description[:-1])} **{cut_account_number}"
+
+    if len(split_description[-1]) == 16:
+        card_number = split_description[-1]
+        cut_card_number_left = card_number[:4] + " " + card_number[4:6]
+        cut_card_number_right = card_number[-4:]
+        return f"{' '.join(split_description[:-1])} {cut_card_number_left}** **** {cut_card_number_right}"
